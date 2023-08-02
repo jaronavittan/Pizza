bf6952c74c6ced6e93a21b94e60bda8a8b97c4c8
Jaron Avittan <96601217+jaronavittan@users.noreply.github.com>
2023-08-02 11:21:31 -0500

    Update .sql

--- .sql
+++ .sql
@@ -1,8 +1,18 @@
  CREATE TABLE pizza_orders (
-    item_id varchar,
+    item_id VARCHAR,
     sku VARCHAR,
-    item_name varchar,
-    item_cat varchar,
+    item_name VARCHAR,
+    item_cat VARCHAR,
     item_size VARCHAR,
     item price INT
  );
+
+
+
+
+
+
+
+SELECT item_name, item_price, sku
+FROM pizza_orders
+WHERE sku IS NULL;
