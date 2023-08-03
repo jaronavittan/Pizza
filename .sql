2b37068ba5ba19367e534e7aa87265413350b063
Jaron Avittan <96601217+jaronavittan@users.noreply.github.com>
2023-08-03 11:59:15 -0500

    Update .sql

--- .sql
+++ .sql
@@ -8,7 +8,13 @@
  );
 
 
-
+INSERT INTO pizza_orders VALUES(1,23,"Margarita","Pizza","Family",10);
+INSERT INTO pizza_orders VALUES(2,22,"Hawaiian","Pizza","Family",10);
+INSERT INTO pizza_orders VALUES(3,66,"Meat lovers","Pizza","Family",10);
+INSERT INTO pizza_orders VALUES(4,67,"Doulbe Cheese","Pizza","Family",10);
+INSERT INTO pizza_orders VALUES(5,12,"Pepperoni","Pizza","Family",10);
+INSERT INTO pizza_orders VALUES(6,23,"Supreme","Pizza","Family",10);
+INSERT INTO pizza_orders VALUES(7,45,"Bacon Ranch","Pizza","Family",10);
 
 
 
