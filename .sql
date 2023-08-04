52dfec6290b25e07b282e432732e794ab8212f88
Jaron Avittan <96601217+jaronavittan@users.noreply.github.com>
2023-08-04 08:05:56 -0500

    Update .sql

--- .sql
+++ .sql
@@ -4,17 +4,17 @@
     item_name VARCHAR,
     item_cat VARCHAR,
     item_size VARCHAR,
-    item price INT
+    item_price INT
  );
 
 
-INSERT INTO pizza_orders VALUES(1,23,"Margarita","Pizza","Family",10);
-INSERT INTO pizza_orders VALUES(2,22,"Hawaiian","Pizza","Family",10);
-INSERT INTO pizza_orders VALUES(3,66,"Meat lovers","Pizza","Family",10);
-INSERT INTO pizza_orders VALUES(4,67,"Doulbe Cheese","Pizza","Family",10);
-INSERT INTO pizza_orders VALUES(5,12,"Pepperoni","Pizza","Family",10);
-INSERT INTO pizza_orders VALUES(6,23,"Supreme","Pizza","Family",10);
-INSERT INTO pizza_orders VALUES(7,45,"Bacon Ranch","Pizza","Family",10);
+INSERT INTO pizza_orders VALUES(1,23,"Margarita","Pizza","Family",8);
+INSERT INTO pizza_orders VALUES(2,22,"Hawaiian","Pizza","Family",11);
+INSERT INTO pizza_orders VALUES(3,66,"Meat lovers","Pizza","Family",15);
+INSERT INTO pizza_orders VALUES(4,67,"Doulbe Cheese","Pizza","Family",9);
+INSERT INTO pizza_orders VALUES(5,12,"Pepperoni","Pizza","Family",9);
+INSERT INTO pizza_orders VALUES(6,23,"Supreme","Pizza","Family",14);
+INSERT INTO pizza_orders VALUES(7,45,"Bacon Ranch","Pizza","Family",11);
 
 
 
@@ -22,3 +22,8 @@ INSERT INTO pizza_orders VALUES(7,45,"Bacon Ranch","Pizza","Family",10);
 SELECT item_name, item_price, sku
 FROM pizza_orders
 WHERE sku IS NULL;
+
+
+SELECT COUNT(item_price)
+FROM pizza_orders
+WHERE item_price >= 10;
