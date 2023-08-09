740cc9968f6bdcb92b51390c7ecb7b5341520660
Jaron Avittan <96601217+jaronavittan@users.noreply.github.com>
2023-08-09 09:21:53 -0500

    Update .sql

--- .sql
+++ .sql
@@ -16,8 +16,22 @@ INSERT INTO pizza_orders VALUES(5,12,"Pepperoni","Pizza","Family",9);
 INSERT INTO pizza_orders VALUES(6,23,"Supreme","Pizza","Family",14);
 INSERT INTO pizza_orders VALUES(7,45,"Bacon Ranch","Pizza","Family",11);
 
+CREATE TABLE toppings (
+  item_id INT,
+  sku INT,
+  item_name VARCHAR,
+  item_category VARCHAR,
+  item_size VARCHAR,
+  item_price INT
+ );
 
-
+INSERT INTO toppings VALUES(1,10,'Mushrooms','topping','Bag',1);
+INSERT INTO toppings VALUES(2,10,'Bacon','topping','Box',1.75);
+INSERT INTO toppings VALUES(3,10,'Tomatoes','topping','Bag',1);
+INSERT INTO toppings VALUES(4,10,'Pepperoni','topping','Box',2);
+INSERT INTO toppings VALUES(5,10,'Corn','topping','Bag',1);
+INSERT INTO toppings VALUES(6,10,'Sausage','topping','Box',3);
+INSERT INTO toppings VALUES(7,10,'Black Olives','topping;,'Bag',1);
 
 SELECT item_name, item_price, sku
 FROM pizza_orders
@@ -37,3 +51,26 @@ WHERE item_price BETWEEN 7 AND 12;
 UPDATE Customers
 SET sku = 17, item_name = 'Onion Soup', item_cat = 'Pizza', item_size = 'Family', item_price = '16'
 WHERE item_id = 4;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
